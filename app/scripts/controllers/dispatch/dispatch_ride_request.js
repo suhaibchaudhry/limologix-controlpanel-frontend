ec027287aaf41730e95de7f52e7ecf574cfd355d
Shyamili H V <shyamily@softwaysolutions.com>
2016-05-20 16:29:11 +0530

    added typeahead to get existing customers and integrated with api

--- app/scripts/controllers/dispatch/dispatch_ride_request.js
+++ app/scripts/controllers/dispatch/dispatch_ride_request.js
@@ -11,11 +11,11 @@ app
     .controller('DispatchRideRequestCtrl', [
         '$scope',
         '$http',
-        'appSettings', 
+        'appSettings',
         '$window',
         'notify',
         'services',
-        function($scope, $http, appSettings, $window, notify,services) {
+        function($scope, $http, appSettings, $window, notify, services) {
             $scope.page = {
                 title: 'Dispatch Ride Request',
                 subtitle: '' //'Place subtitle here...'
@@ -26,19 +26,37 @@ app
                 last_name: '',
                 email: '',
                 mobile_number: '',
-                organisation:''
+                organisation: ''
             }
             $scope.selected = '';
-            $scope.states = [{postcode:'B1',address:'Bull ring'},{postcode:'B1',address:'Bull ring'},{postcode:'B1',address:'Bull ring'},{postcode:'B13',address:'shyam'},{postcode:'B1',address:'Bull ring'},{postcode:'M1',address:'Manchester'}];
+            $scope.noresults = false;
+            $scope.loadingcustomers = false;
 
             $scope.getExistingCustomers = function(search_string) {
                 var url = appSettings.serverPath + appSettings.serviceApis.getExistingCustomers;
-                services.funcPostRequest(url,{'search_string':search_string}).then(function(response) {
-                        console.log('searched customer',response);
-                        notify({ classes: 'alert-success', message: response.message });
-                    },function(error){
-                        notify({ classes: 'alert-danger', message: response.message });  
-                    });
+                $scope.loadingcustomers = true;
+                return services.funcPostRequest(url, { 'search_string': search_string }).then(function(response) {
+                    if (response.data) {
+                        $scope.noresults = false;
+                        $scope.loadingcustomers = false;
+                        var customers = response.data.customers;
+                        var results = [];
+                        angular.forEach(customers, function(item) {
+                            item.full_name = item.first_name + " " + item.last_name;
+                            if (item.full_name.toLowerCase().indexOf(search_string.toLowerCase()) > -1) {
+                                results.push(item);
+                            }
+                        });
+                        return results;
+                    } else {
+                        $scope.noresults = true;
+                        $scope.loadingcustomers = false;
+                        jQuery('#nocustomer').text(response.message);
+                    }
+                    //notify({ classes: 'alert-success', message: response.message });
+                }, function(error) {
+                    notify({ classes: 'alert-danger', message: response.message });
+                });
             }
 
             // function to submit the form after all validation has occurred
@@ -49,17 +67,17 @@ app
                         last_name: $scope.customerInfo.last_name,
                         email: $scope.customerInfo.email,
                         mobile_number: $scope.customerInfo.mobile_number,
-                        organisation:$scope.customerInfo.organisation
+                        organisation: $scope.customerInfo.organisation
                     };
                     var customerDetails = {
                         "auth_token": $window.sessionStorage['token'],
                         "customer": customer
                     }
                     var url = appSettings.serverPath + appSettings.serviceApis.addcustomer;
-                    services.funcPostRequest(url, customerDetails).then(function(response){
-                      notify({ classes: 'alert-success', message: response.message });
-                    },function(error,status){
-                      notify({ classes: 'alert-danger', message: response.message });
+                    services.funcPostRequest(url, customerDetails).then(function(response) {
+                        notify({ classes: 'alert-success', message: response.message });
+                    }, function(error, status) {
+                        notify({ classes: 'alert-danger', message: response.message });
                     })
                 } else {
 
@@ -67,4 +85,41 @@ app
             };
         }
     ])
+    .controller('DatepickerDemoCtrl', function ($scope) {
 
+        $scope.today = function() {
+          $scope.dt = new Date();
+        };
+
+        $scope.today();
+
+        $scope.clear = function () {
+          $scope.dt = null;
+        };
+
+        // Disable weekend selection
+        $scope.disabled = function(date, mode) {
+          return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+        };
+
+        $scope.toggleMin = function() {
+          $scope.minDate = $scope.minDate ? null : new Date();
+        };
+        $scope.toggleMin();
+
+        $scope.open = function($event) {
+          $event.preventDefault();
+          $event.stopPropagation();
+
+          $scope.opened = true;
+        };
+
+        $scope.dateOptions = {
+          formatYear: 'yy',
+          startingDay: 1,
+          'class': 'datepicker'
+        };
+
+        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+        $scope.format = $scope.formats[0];
+    })
