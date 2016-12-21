dbdf4f21ed8bdd0161327be9c6692a274a072c5e
Asad Hasan <asadpakistani@gmail.com>
2016-12-21 14:35:16 -0600

    Map running

--- app/scripts/controllers/dispatch/drivermap.js
+++ app/scripts/controllers/dispatch/drivermap.js
@@ -4,91 +4,21 @@ app
   .controller('DriverMapCtrl', function ($scope) {
 
     $scope.page = {
-      title: 'Leaflet Maps',
-      subtitle: 'Place subtitle here...'
+      title: 'Drivers\' Map',
+      subtitle: 'This map refreshes preodically as data is recieved from drivers'
     };
 
   })
 
-  .controller("leafletMap1", [ "$scope", function($scope) {
-
-  }])
-
-  .controller("leafletMap2", [ "$scope", function($scope) {
-    angular.extend($scope, {
-      autodiscover: {
-        autoDiscover: true
-      }
-    });
-  }])
-
-  .controller("leafletMap3", [ "$scope", "leafletData", function($scope, leafletData) {
-
-    L.Icon.Default.imagePath = 'styles/images';
+  .controller("leafletMap4", [ "$scope", "$http", function($scope) {
 
     angular.extend($scope, {
-      london: {
-        lat: 51.505,
-        lng: -0.09,
-        zoom: 4
-      },
-      controls: {
-        draw: {}
-      },
-      layers: {
-        baselayers: {
-          mapbox_light: {
-            name: 'Mapbox Light',
-            url: 'http://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={apikey}',
-            type: 'xyz',
-            layerOptions: {
-              apikey: 'pk.eyJ1IjoiYnVmYW51dm9scyIsImEiOiJLSURpX0pnIn0.2_9NrLz1U9bpwMQBhVk97Q',
-              mapid: 'bufanuvols.lia22g09'
-            },
-            layerParams: {
-              showOnSelector: false
-            }
-          }
-        },
-        overlays: {
-          draw: {
-            name: 'draw',
-            type: 'group',
-            visible: true,
-            layerParams: {
-              showOnSelector: false
-            }
-          }
-        }
-      }
-    });
-
-
-    leafletData.getMap("map3").then(function(map) {
-      leafletData.getLayers("map3").then(function(baselayers) {
-        var drawnItems = baselayers.overlays.draw;
-        map.on('draw:created', function (e) {
-          var layer = e.layer;
-          drawnItems.addLayer(layer);
-          console.log(JSON.stringify(layer.toGeoJSON()));
-        });
-      });
-    });
-  }])
-
-  .controller("leafletMap4", [ "$scope", function($scope) {
-
-    angular.extend($scope, {
-      berlin: {
-        lat: 52.52,
-        lng: 13.40,
-        zoom: 14
+      houston: {
+        lat: 29.7630556,
+        lng: -95.3630556,
+        zoom: 10
       },
       markers: {
-        m1: {
-          lat: 52.52,
-          lng: 13.40
-        }
       },
       layers: {
         baselayers: {
@@ -110,52 +40,40 @@ app
         }
       }
     });
+    $scope.removeMarkers = function() {
+        $scope.markers = {};
+    }
 
-  }])
-
-  .controller("leafletMap5", ["$scope", "$http", function($scope, $http) {
-
-    var points = [];
-    var heatmap = {
-      name: 'Heat Map',
-      type: 'heat',
-      data: points,
-      visible: true
+    $scope.addMarkers = function() {
+      var xmlHttp = new XMLHttpRequest();
+      xmlHttp.onreadystatechange = function() {
+          if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+              //callback(xmlHttp.responseText);
+              $scope.removeMarkers();
+              var drivers = JSON.parse(xmlHttp.responseText);
+              var markers = {};
+              for(var d in drivers) {
+                markers[drivers[d].driver_id] = {
+                  lat: drivers[d].latlon.x,
+                  lng: drivers[d].latlon.y
+                };
+              }
+              angular.extend($scope, {
+                  markers: markers
+              });
+          }
+      }
+      xmlHttp.open("GET", 'http://limologix.softwaystaging.com/drivermap', true); // true for asynchronous
+      xmlHttp.send(null);
     };
 
-    $http.get("scripts/jsons/heat-points.json").success(function(data) {
-      $scope.layers.overlays = {
-        heat: {
-          name: 'Heat Map',
-          type: 'heat',
-          data: data,
-          layerOptions: {
-            radius: 20,
-            blur: 10
-          },
-          visible: true
-        }
-      };
-    });
-
-    angular.extend($scope, {
-      center: {
-        lat: 37.774546,
-        lng: -122.433523,
-        zoom: 12
-      },
-      layers: {
-        baselayers: {
-          mapbox_light: {
-            name: 'Mapbox Light',
-            url: 'http://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={apikey}',
-            type: 'xyz',
-            layerOptions: {
-              apikey: 'pk.eyJ1IjoiYnVmYW51dm9scyIsImEiOiJLSURpX0pnIn0.2_9NrLz1U9bpwMQBhVk97Q',
-              mapid: 'bufanuvols.lia22g09'
-            }
-          }
-        }
-      }
-    });
+    if($scope.interval) {
+      //console.log("Clear Interval");
+      clearInterval($scope);
+    }
+    $scope.addMarkers();
+    $scope.interval = setInterval(function() {
+      //console.log("Running interval");
+      $scope.addMarkers();
+    }, 5000);
   }]);
